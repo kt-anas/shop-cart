a6340ac18c8192e4f5ef3e7db3bf9f96e2771ad6
anas <ktmanas2020@gmail.com>
2024-11-02 20:34:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+ 
+ 
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+  
+        <div className="bg-red-700 text-7xl">
+            <h1 className="text-white">Hello</h1>
+        </div>
+    </>
+  )
+}
+
+export default App

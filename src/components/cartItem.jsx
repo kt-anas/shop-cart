841dbe52c3f0492e4d7b77c9590b6ac8b21baedd
anas <ktmanas2020@gmail.com>
2025-02-06 22:08:28 +0530

    cart

--- src/components/cartItem.jsx
+++ src/components/cartItem.jsx
@@ -1,10 +1,10 @@
-import React, {useState, useEffect} from 'react' 
-import { products } from '../products';
+import React, { useState, useEffect } from 'react'
+import { products } from '../data/products';
 import { useDispatch } from 'react-redux';
 import { changeQuantity } from '../stores/cart';
 
 const CartItem = (props) => {
-    const {productId, quantity} = props.data;
+    const { productId, quantity } = props.data;
     const [detail, setDetail] = useState([]);
     const dispatch = useDispatch();
     useEffect(() => {
@@ -23,18 +23,18 @@ const CartItem = (props) => {
             quantity: quantity + 1
         }));
     }
-  return (
-    <div className='flex justify-between items-center bg-slate-600 text-white p-2 border-b-2 border-slate-700 gap-5 rounded-md'>
-        <img src={detail.image} alt="" className='w-12'/>
-        <h3>{detail.name}</h3>
-        <p>${detail.price * quantity}</p>
-        <div className='w-20 flex justify-between gap-2'>
-            <button className='bg-gray-200 rounded-full w-6 h-6 text-cyan-600' onClick={handleMinusQuantity}>-</button>
-            <span>{quantity}</span>
-            <button className='bg-gray-200 rounded-full w-6 h-6 text-cyan-600' onClick={handlePlusQuantity}>+</button>
+    return (
+        <div className='flex justify-between items-center bg-slate-600 text-white p-2 border-b-2 border-slate-700 gap-5 rounded-md'>
+            <img src={detail.image} alt="" className='w-12' />
+            <h3>{detail.name}</h3>
+            <p>${detail.price * quantity}</p>
+            <div className='w-20 flex justify-between gap-2'>
+                <button className='bg-gray-200 rounded-full w-6 h-6 text-cyan-600' onClick={handleMinusQuantity}>-</button>
+                <span>{quantity}</span>
+                <button className='bg-gray-200 rounded-full w-6 h-6 text-cyan-600' onClick={handlePlusQuantity}>+</button>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default CartItem
